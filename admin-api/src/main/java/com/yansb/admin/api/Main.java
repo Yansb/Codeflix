df0bceb51bcefd75461931a5f968c4152cf87132
Yansb <yansbarreiro@gmail.com>
2022-07-14 23:53:45 -0300

    Initial commit

---
+++ admin-api/src/main/java/com/yansb/admin/api/Main.java
@@ -0,0 +1,7 @@
+package com.yansb.admin.api;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Jesus amado finalmente q inferno fazer essa porra funcionar");
+  }
+}
