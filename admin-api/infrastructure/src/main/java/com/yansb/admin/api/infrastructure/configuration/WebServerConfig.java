caf3bb7ffc549cbcefe4877726a029b8a2c2c186
yansb <yansbarreiro@gmail.com>
2022-07-18 23:22:52 -0300

    feat: configure spring boot

---
+++ admin-api/infrastructure/src/main/java/com/yansb/admin/api/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.yansb.admin.api.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.yansb.admin.api")
+public class WebServerConfig {
+}
