6d05b3cf3adcce42a9b55a0829e859e3eb72bdd4
yansb <yansbarreiro@gmail.com>
2022-08-15 09:55:50 -0300

    chore: update mysql clean up extension

---
+++ admin-api/infrastructure/src/test/java/com/yansb/admin/api/MySQLCleanUpExtension.java
@@ -0,0 +1,28 @@
+package com.yansb.admin.api;
+
+import com.yansb.admin.api.infrastructure.category.persistence.CategoryRepository;
+import com.yansb.admin.api.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MySQLCleanUpExtension implements BeforeEachCallback {
+
+  @Override
+  public void beforeEach(final ExtensionContext context) {
+    final var appContext = SpringExtension.getApplicationContext(context);
+
+    cleanUp(List.of(
+        appContext.getBean(GenreRepository.class),
+        appContext.getBean(CategoryRepository.class)
+    ));
+  }
+
+  private void cleanUp(final Collection<CrudRepository> repositories) {
+    repositories.forEach(CrudRepository::deleteAll);
+  }
+}
