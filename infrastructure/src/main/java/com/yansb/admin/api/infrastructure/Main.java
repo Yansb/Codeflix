7b8ee455690fd8b1091d566237f96d1683d57595
yansb <yansbarreiro@gmail.com>
2023-06-26 18:26:07 -0300

    feat: elk

--- infrastructure/src/main/java/com/yansb/admin/api/infrastructure/Main.java
+++ infrastructure/src/main/java/com/yansb/admin/api/infrastructure/Main.java
@@ -1,14 +1,20 @@
 package com.yansb.admin.api.infrastructure;
 
 import com.yansb.admin.api.infrastructure.configuration.WebServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOG.info("[step:to-be-init] Inicializando o Spring");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
+        LOG.info("[step:initialized] Spring inicializado com sucesso");
     }
 }
--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,11 +1,16 @@
 # Criar as docker networks
 docker network create adm_videos_services
+docker network create elastic
 
 # Criar as pastas com permissões
+sudo chown root app/filebeat/filebeat.docker.yml
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
+mkdir -m 777 .docker/filebeat
 
-docker compose -f app/docker-compose.yml up -d
+docker compose -f services/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
 
 echo "Starting 🐋️ containers..."
 sleep 20
